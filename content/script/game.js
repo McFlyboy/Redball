11dccfb5b63abe5d28d0af6580c8d5fdd47c6bab
Henrik <hsn95@live.com>
2018-10-31 01:55:09 +0100

    Added a high-score
    The high-score is stored as a cookie

--- content/script/game.js
+++ content/script/game.js
@@ -3,6 +3,7 @@ const target = document.getElementById("target");
 const innerTimer = document.getElementById("inner-timer");
 const scoreText = document.getElementById("score");
 const infoText = document.getElementsByTagName("h1")[0];
+const highscoreText = document.getElementById("highscore");
 const backButton = document.getElementById("back-button");
 document.body.style.backgroundColor = bgColor;
 document.body.style.color = textColor;
@@ -15,19 +16,13 @@ const targetTime = 100;
 let gameState = gameStates.START;
 let score;
 let lastClickTime;
-let interval = setInterval(function() {
+setInterval(function() {
 		if(gameState === gameStates.RUN) {
 			timeRemaining = Math.floor((new Date().getTime() - lastClickTime) * 0.1);
 			innerTimer.style.width = timeRemaining + "%";
 			innerTimer.style.height = timeRemaining + "%";
 			if(timeRemaining >= targetTime){
-				gameState = gameStates.GAME_OVER;
-				infoText.innerHTML = "GAME OVER";
-				backButton.style.display = "block";
-				target.style.left = "50%";
-				target.style.bottom = "50%";
-				innerTimer.style.width = "0%";
-				innerTimer.style.height = "0%";
+				setGameOver();
 			}
 		}
 }, 1);
@@ -35,6 +30,7 @@ function hitTarget() {
 	if(gameState != gameStates.RUN){
 		gameState = gameStates.RUN;
 		infoText.innerHTML = "";
+		highscoreText.innerHTML = "";
 		backButton.style.display = "none";
 		score = 0;
 	}
@@ -43,4 +39,28 @@ function hitTarget() {
 	score++;
 	scoreText.innerHTML = score;
 	lastClickTime = new Date().getTime();
+}
+function setGameOver() {
+	const lastHighscore = getCookie("highscore");
+	if(lastHighscore === "") {
+		setCookie("highscore", score, 30);
+		highscoreText.innerHTML = "New high-score!";
+	}
+	else {
+		let lastHighscoreInt = parseInt(lastHighscore);
+		if(score > lastHighscoreInt) {
+			setCookie("highscore", score, 30);
+			highscoreText.innerHTML = "New high-score!";
+		}
+		else {
+			highscoreText.innerHTML = "High-score: " + lastHighscoreInt;
+		}
+	}
+	gameState = gameStates.GAME_OVER;
+	infoText.innerHTML = "GAME OVER";
+	backButton.style.display = "block";
+	target.style.left = "50%";
+	target.style.bottom = "50%";
+	innerTimer.style.width = "0%";
+	innerTimer.style.height = "0%";
 }
--- content/script/menu.js
+++ content/script/menu.js
@@ -4,3 +4,8 @@ document.querySelector("#red").style.color = buttonColor;
 const button = document.querySelector(".button");
 button.style.color = innerTextColor;
 button.style.backgroundColor = buttonColor;
+const highscoreText = document.getElementById("highscore");
+const lastHighscore = getCookie("highscore");
+if(lastHighscore != "") {
+	highscoreText.innerHTML = "Welcome back!<br><br>High-score: " + lastHighscore;
+}
