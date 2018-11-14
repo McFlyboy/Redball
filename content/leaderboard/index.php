4dbb086079483f7d387a0c3b36a1ba49b971438e
Henrik <hsn95@live.com>
2018-11-14 05:17:05 +0100

    Leaderboard working

--- content/leaderboard/index.php
+++ content/leaderboard/index.php
@@ -1,3 +1,21 @@
 <?php
-	echo "Test";
+	$servername = "sql31.webhuset.no";
+	$username = "89882_redball_db";
+	$password = "MaiWaifu<3";
+	$dbname = "89882_redball_db";
+	$connection = new mysqli($servername, $username, $password, $dbname);
+	if($connection->connect_error) {
+		die("Connection failed: " . $connection->connect_error);
+	}
+	$sql = "SELECT * FROM `Leaderboard`";
+	$result = $connection->query($sql);
+	if($result->num_rows > 0){
+		while($row = $result->fetch_assoc()) {
+			echo "<p>Name: " . $row["Name"] . " - Score: " . $row["Score"] . " - Date: " . $row["Date"] . "</p>";
+		}
+	}
+	else {
+		 echo "<p>0 results</p>";
+	}
+	$connection->close();
 ?>
