3e25b4f5d2656371762753352ed9d9cd3dbb176f
Henrik <hsn95@live.com>
2018-11-15 22:02:49 +0100

    Leaderboard ordered by Score

--- content/leaderboard/index.php
+++ content/leaderboard/index.php
@@ -7,7 +7,7 @@
 	if($connection->connect_error) {
 		die("Connection failed: " . $connection->connect_error);
 	}
-	$sql = "SELECT * FROM `Leaderboard`";
+	$sql = "SELECT * FROM `Leaderboard` ORDER BY `Score` DESC";
 	$result = $connection->query($sql);
 	if($result->num_rows > 0){
 		while($row = $result->fetch_assoc()) {
