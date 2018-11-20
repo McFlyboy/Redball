24e76a86ffdcd3abfd861b0fecfa111c9d61760e
Henrik <hsn95@live.com>
2018-11-20 03:59:17 +0100

    More work on leaderboard

--- content/leaderboard/index.php
+++ content/leaderboard/index.php
@@ -1,21 +1,63 @@
-<?php
-	$servername = "sql31.webhuset.no";
-	$username = "89882_redball_db";
-	$password = "";
-	$dbname = "89882_redball_db";
-	$connection = new mysqli($servername, $username, $password, $dbname);
-	if($connection->connect_error) {
-		die("Connection failed: " . $connection->connect_error);
-	}
-	$sql = "SELECT * FROM `Leaderboard` ORDER BY `Score` DESC";
-	$result = $connection->query($sql);
-	if($result->num_rows > 0){
-		while($row = $result->fetch_assoc()) {
-			echo "<p>Name: " . $row["Name"] . " - Score: " . $row["Score"] . " - Date: " . $row["Date"] . "</p>";
-		}
-	}
-	else {
-		 echo "<p>0 results</p>";
-	}
-	$connection->close();
-?>
+<!doctype html>
+<html lang="en-US">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="../shared-res/style/meyers-reset.css">
+		<link rel="stylesheet" href="../shared-res/style/default.css">
+		<link rel="stylesheet" href="leaderboard.css">
+		<title>Redball. Leaderboard</title>
+	</head>
+	<body>
+		<section class="grid-container">
+			<div class="grid-item">
+				<p>#</p>
+			</div>
+			<div class="grid-item">
+				<p>Name</p>
+			</div>
+			<div class="grid-item">
+				<p>Score</p>
+			</div>
+			<div class="grid-item">
+				<p>Date</p>
+			</div>
+			<?php
+				$servername = "sql31.webhuset.no";
+				$username = "89882_redball_db";
+				$password = "";
+				$dbname = "89882_redball_db";
+				$connection = new mysqli($servername, $username, $password, $dbname);
+				if($connection->connect_error) {
+					die("Connection failed: " . $connection->connect_error);
+				}
+				$sql = "SELECT * FROM `Leaderboard` ORDER BY `Score` DESC";
+				$result = $connection->query($sql);
+				if($result->num_rows > 0){
+					$i = 1;
+					while($row = $result->fetch_assoc()) {
+						echo 
+								"<div class=\"grid-item\">" . 
+									"<p>" . $i . "</p>" . 
+								"</div>" . 
+								"<div class=\"grid-item\">" . 
+									"<p>" . $row["Name"] . "</p>" . 
+								"</div>" . 
+								"<div class=\"grid-item\">" . 
+									"<p>" . $row["Score"] . "</p>" . 
+								"</div>" . 
+								"<div class=\"grid-item\">" . 
+									"<p>" . $row["Date"] . "</p>" . 
+								"</div>";
+						$i++;
+					}
+				}
+				else {
+					echo "<p>0 results</p>";
+				}
+				$connection->close();
+			?>
+		</section>
+		<a class="button" id="back-button" href="../">Back to Menu</a>
+	</body>
+</html>
