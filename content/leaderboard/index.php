5fada32fbb4fc6d6edef7b8322ba4c60e9969924
Henrik <hsn95@live.com>
2018-11-22 04:40:37 +0100

    Database connection-info stored in separate file

--- content/leaderboard/index.php
+++ content/leaderboard/index.php
@@ -23,10 +23,7 @@
 				<p>Date</p>
 			</div>
 			<?php
-				$servername = "sql31.webhuset.no";
-				$username = "89882_redball_db";
-				$password = "";
-				$dbname = "89882_redball_db";
+				require '../shared-res/server-script/db-vars.php';
 				$connection = new mysqli($servername, $username, $password, $dbname);
 				if($connection->connect_error) {
 					die("Connection failed: " . $connection->connect_error);
---
+++ content/shared-res/server-script/db-vars.php
@@ -0,0 +1,6 @@
+<?php
+	$servername = "sql31.webhuset.no";
+	$username = "89882_redball_db";
+	$password = "";
+	$dbname = "89882_redball_db";
+?>
